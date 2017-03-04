353beb3a75514d84f352b8626e7e8e03b6e171b8
Meskó László <mesko.laszlo@pontsystems.eu>
2017-03-04 22:12:02 +0100

    game finish on timeout and game restart after winner

--- trivia.py
+++ trivia.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
-from enum import enum
+from enum import Enum
+
+class Idomero:
+    elapsedtime = 0
+    maxtime = 0
+    def __init__(self, maxtime):
+        self.maxtime = maxtime
+    
+    def addanswertime(self, elapsedtime):
+        self.elapsedtime += elapsedtime
+        return self.maxtime<=elapsedtime
+    
+
 class QuestionType(Enum):
      Pop = 'Pop'
      Science = 'Science'
@@ -9,7 +21,7 @@ class QuestionType(Enum):
      History = 'Hist'
 
 class QuestionBank:
-    qindexlist = dict()
+    currentindexlist = dict()
     qlist = dict()
     def create_question(self, questiontype, question):
         l = self.qlist.get(questiontype, list())
@@ -17,45 +29,56 @@ class QuestionBank:
         self.qlist[questiontype] = l
     
     def get_next_question(self, questiontype):
-        self.qindexlist[questiontype] = self.qindexlist.get(questiontype, -1) + 1
+        self.currentindexlist[questiontype] = self.currentindexlist.get(questiontype, -1) + 1
         l = self.qlist.get(questiontype, list())
-        if self.qindexlist[questiontype]>=len(l):
-            self.qindexlist[questiontype] = 0
-        return l[self.qindexlist[questiontype]]
+        if self.currentindexlist[questiontype]>=len(l):
+            self.currentindexlist[questiontype] = 0
+            print 'kerdes:', self.currentindexlist[questiontype], len(l)
+        return l[self.currentindexlist[questiontype]]
 
     
 class Board:
     fields = list()
     def __init__(self):
-        for t in [Pop, Science, Sports, Rock, History, Science, Sports, Rock, History, Science, Sports, Rock]:
-            fields.append(t)
+        for t in [QuestionType.Pop, QuestionType.Science, QuestionType.Sports, QuestionType.Rock, \
+                  QuestionType.History, QuestionType.Science, QuestionType.Sports, QuestionType.Rock, \
+                  QuestionType.History, QuestionType.Science, QuestionType.Sports, QuestionType.Rock]:
+            self.fields.append(t.value)
 
     def get_field_type(self, index):
-        return self.fields(index%len(self.fields))
+        return self.fields[index%len(self.fields)]
 
     def get_board_size(self):
         return len(self.fields)
 
+
+
 class Game:
-    def __init__(self):
+    qbank = None
+    board = None
+    def __init__(self, board, questionbank):
         self.players = []
         self.places = [0] * 6
         self.purses = [0] * 6
         self.in_penalty_box = [0] * 6
-        
-        self.pop_questions = []
-        self.science_questions = []
-        self.sports_questions = []
-        self.rock_questions = []
-        
+                
         self.current_player = 0
         self.is_getting_out_of_penalty_box = False
         
-        for i in range(50):
-            self.pop_questions.append("Pop Question %s" % i)
-            self.science_questions.append("Science Question %s" % i)
-            self.sports_questions.append("Sports Question %s" % i)
-            self.rock_questions.append(self.create_rock_question(i))
+        if questionbank == None:
+            self.qbank = QuestionBank()
+            for i in range(50):
+                for qType in QuestionType:
+                    qt= qType.value #repr(qType).split(".")[1]
+                    self.qbank.create_question(qt, qt+' Question '+str(i))
+#                    print 'Added '+qt, qt+' Question '+str(i)
+        else:
+            self.qbank = questionbank
+        
+        if board == None:
+            self.board = Board()
+        else:
+            self.board = board
     
     def create_rock_question(self, index):
         return "Rock Question %s" % index
@@ -111,10 +134,10 @@ class Game:
             self._ask_question()
     
     def _ask_question(self):
-        if self._current_category == 'Pop': print self.pop_questions.pop(0)
-        if self._current_category == 'Science': print self.science_questions.pop(0)
-        if self._current_category == 'Sports': print self.sports_questions.pop(0)
-        if self._current_category == 'Rock': print self.rock_questions.pop(0)
+        q = self.qbank.get_next_question(self.board.get_field_type(self.places[self.current_player]))
+        print q
+        
+
     
     @property
     def _current_category(self):
@@ -178,7 +201,17 @@ class Game:
     def _did_player_win(self):
         return not (self.purses[self.current_player] == 6)
 
-
+    def close_game(self, reason):
+        # 1 embernek van-e max pontja?
+        # ha igen, akkor o a gyoztes.
+        # ha nem toroljuk a nem-gyozteseket
+        # gyozelem feltetele megvaltozik, hogy a kor vegen kinek vna a legtobb pontja
+        maxval=max(self.purses)
+        maxindex=self.purses.index(maxval)
+        print reason + self.players[maxindex]
+        
+    
+    
 from random import randrange
 
 if __name__ == '__main__':
@@ -190,12 +223,14 @@ if __name__ == '__main__':
     game.add('Pat')
     game.add('Sue')
 
-    dice_trows = [6,6,6,1,3,1,6,4,1,2,4,6,1,2,3,4,2,3,1,1,6,1,6,6,3,4,6,4,4,2,6,4,2,5,5,4,6,4,1,6,5,5,5,5,1,2,3,3,4,4]
+    dice_trows =    [6,6,6,1,3,1,6,4,1,2,4,6,1,2,3,4,2,3,1,1,6,1,6,6,3,4,6,4,4,2,6,4,2,5,5,4,6,4,1,6,5,5,5,5,1,2,3,3,4,4]
     given_answers = [6,9,2,4,7,0,5,1,0,2,8,4,2,3,8,1,2,5,1,7,8,4,5,7,8,1,3,1,9,2,9,6,2,2,1,0,2,6,1,2,8,9,7,1,3,5,2,5,1,3]
+    given_time =    [1,2,3,4,5,4,3,3,9,9,9,1,1,9,9,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-    test_game_states = zip(dice_trows, given_answers)
+    test_game_states = zip(dice_trows, given_answers, given_time)
+    idomero = Idomero(30)
 
-    for (throw, answer) in test_game_states:
+    for (throw, answer, time) in test_game_states:
         game.roll(throw)
 
         if answer == 7:
@@ -204,6 +239,9 @@ if __name__ == '__main__':
             not_a_winner = game.was_correctly_answered()
 
         if not not_a_winner: break
+
+        
+            
 
     # while True:
     #     game.roll(randrange(5) + 1)
--- trivia_test.py
+++ trivia_test.py
@@ -11,28 +11,43 @@ class test1(TestCase):
     def test_one(self):
         sys.stdout = open('reference/test_result.txt', 'w')
 
-        game = trivia.Game()
+        idomero=trivia.Idomero(30)
+        
+        game = trivia.Game(None, None)
 
         game.add('Chet')
         game.add('Pat')
         game.add('Sue')
 
-        dice_trows = [6, 6, 6, 1, 3, 1, 6, 4, 1, 2, 4, 6, 1, 2, 3, 4, 2, 3, 1, 1, 6, 1, 6, 6, 3, 4, 6, 4, 4, 2, 6, 4, 2,
-                      5, 5, 4, 6, 4, 1, 6, 5, 5, 5, 5, 1, 2, 3, 3, 4, 4]
-        given_answers = [6, 9, 2, 4, 7, 0, 5, 1, 0, 2, 8, 4, 2, 3, 8, 1, 2, 5, 1, 7, 8, 4, 5, 7, 8, 1, 3, 1, 9, 2, 9, 6,
-                         2, 2, 1, 0, 2, 6, 1, 2, 8, 9, 7, 1, 3, 5, 2, 5, 1, 3]
+        dice_trows =    [6,6,6,1,3,1,6,4,1,2,4,6,1,2,3,4,2,3,1,1,6,1,6,6,3,4,6,4,4,2,6,4,2,5,5,4,6,4,1,6,5,5,5,5,1,2,3,3,4,4]
+        given_answers = [6,9,2,4,7,0,5,1,0,2,8,4,2,3,8,1,2,5,1,7,8,4,5,7,8,1,3,1,9,2,9,6,2,2,1,0,2,6,1,2,8,9,7,1,3,5,2,5,1,3]
+        given_time =    [1,2,3,4,5,4,3,3,9,9,9,1,1,9,9,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-        test_game_states = zip(dice_trows, given_answers)
+        test_game_states = zip(dice_trows, given_answers, given_time)
 
-        for (throw, answer) in test_game_states:
+        for (throw, answer, time) in test_game_states:
             game.roll(throw)
 
+            timeout = idomero.addanswertime(time)
             if answer == 7:
                 not_a_winner = game.wrong_answer()
             else:
                 not_a_winner = game.was_correctly_answered()
 
-            if not not_a_winner: break
+            ## nyert valaki / lejart az ido / megy tovabb
+            if timeout:
+                game.close_game('Winner on timeout: ')
+                # gyoztes kihirdetese
+            elif not not_a_winner:
+                # uj jatek a vesztesekkel + az uj jatekossal
+                # Erre meg nem keszult teszt
+                game.close_game('Winner on max points: ')
+                game = trivia.Game(None, None)
+                game.add('Sue')
+                game.add('Pat')
+                game.add('Bob')
+                
+                
 
         sys.stdout.close()
 
@@ -40,3 +55,6 @@ class test1(TestCase):
         test_result = open('reference/test_result.txt', 'r').read()
 
         self.assertEqual(test_file, test_result)
+
+
+
